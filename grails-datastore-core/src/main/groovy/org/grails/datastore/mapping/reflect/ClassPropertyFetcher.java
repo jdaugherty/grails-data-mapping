670c167babe70a21ee4afe42014040f426add64a
James Daugherty <jdaugherty@jdresources.net>
2025-01-15 23:51:04 -0500

    Fix javadoc

--- grails-datastore-core/src/main/groovy/org/grails/datastore/mapping/reflect/ClassPropertyFetcher.java
+++ grails-datastore-core/src/main/groovy/org/grails/datastore/mapping/reflect/ClassPropertyFetcher.java
@@ -111,7 +111,7 @@ public class ClassPropertyFetcher {
     }
 
     /**
-     * @Deprecated will be removed in a future version of GORM
+     * @deprecated will be removed in a future version of GORM
      */
     @Deprecated
     public Object getReference() {
